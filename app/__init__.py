70072d958c649853bb16048d6e995d14379aabef
JesusGar13 <jgb1007@alu.ubu.es>
2024-10-15 20:13:10 +0200

    Creación de la estructura web de la app
    Se va añadir los htmls de la web y su navegacion de forma sencilla

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,30 @@
-# app/__init__.py
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
 
     @app.route('/')
     def home():
-        return "¡Bienvenido a mi aplicación Flask!"
+        return render_template('main.html')
+
+    @app.route('/login')
+    def login():
+        return render_template('login.html')
+
+    @app.route('/register')
+    def register():
+        return render_template('register.html')
+
+    @app.route('/tracking')
+    def tracking():
+        return render_template('tracking.html')
+
+    @app.route('/tracking/track_session')
+    def track_session():
+        return render_template('track_session.html')
+
+    @app.route('/denied_web')
+    def denied_web():
+        return render_template('denied_web.html')
 
     return app
