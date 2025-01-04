f486651d3a89bc486d1399e3aee651cf13796dec
Tibbitea <62241263+Tibbitea@users.noreply.github.com>
2025-01-04 18:51:28 +0100

    imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from app.utils import UserManager
 from app.utils.SingleConexionBD import SingleConexionBD
 
