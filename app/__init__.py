14aeb5a4672b43b8e94be49039e759794b2b780d
JesusGar13 <jgb1007@alu.ubu.es>
2025-01-04 18:30:56 +0100

    Refactorizacion: Extracion de la logica de login, register y logout en la clase userManager

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,15 @@
-from flask import Flask, render_template
-from flask import request, flash, redirect, url_for 
-from app.utils.SingleConexionBD import SingleConexionBD 
-import re
-
-
+from flask import Flask, render_template, request, redirect, url_for
+from app.utils.UserManager import UserManager
+from app.utils.SingleConexionBD import SingleConexionBD
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = 'your_secret_key'  
+    app.secret_key = 'your_secret_key'
 
     db = SingleConexionBD()
     db.create_tablas()
     db.delete_all_users()
+    user_manager = UserManager()
 
     @app.route('/')
     def home():
@@ -22,13 +20,8 @@ def create_app():
         if request.method == 'POST':
             username = request.form['username']
             password = request.form['password']
-            
-            user = db.verify_user(username, password)
-            if user:
+            if user_manager.login_user(username, password):
                 return redirect(url_for('user_main'))
-            else:
-                flash('Usuario o contraseña incorrectos')
-                return render_template('login.html')
         return render_template('login.html')
 
     @app.route('/register', methods=['GET', 'POST'])
@@ -37,45 +30,18 @@ def create_app():
             username = request.form['username']
             email = request.form['email']
             password = request.form['password']
-
-            if not validate_password(password):
-                flash('La contraseña debe tener entre 6 y 12 caracteres, al menos una letra mayúscula, una minúscula y un número')
-                return render_template('register.html')
-
-            if not validate_email(email):
-                flash('Email incorrecto. Debe ser de la forma: ----@----.es o ----@----.com')
-                return render_template('register.html')    
-            
-            if db.insert_newUser(username, email, password):
-                flash('Usuario registrado correctamente. ¡Bienvenid@! Inicia sesión para continuar')
+            if user_manager.register_user(username, email, password):
                 return redirect(url_for('login'))
-            else:
-                flash('Error al registrar el usuario')
-                return render_template('register.html')
-        
         return render_template('register.html')
 
-
-    def validate_email(email):
-            pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(es|com)$'
-            return re.match(pattern, email) is not None
-
-
-    def validate_password(password):
-        if len(password) < 6 or len(password) > 12:
-            return False
-        if not re.search("[a-z]", password):
-            return False
-        if not re.search("[A-Z]", password):
-            return False
-        if not re.search("[0-9]", password):
-            return False
-        return True
+    @app.route('/logout')
+    def logout():
+        user_manager.logout_user()
+        return redirect(url_for('home'))
 
     @app.route('/user_main')
     def user_main():
-        return render_template('user_main.html')    
-
+        return render_template('user_main.html')
 
     @app.route('/tracking')
     def tracking():
@@ -89,13 +55,4 @@ def create_app():
     def denied_web():
         return render_template('denied_web.html')
 
-    @app.route('/logout', methods=['GET', 'POST'])
-    def logout():
-        if request.method == 'POST':
-            # Aquí puedes agregar la lógica para cerrar la sesión del usuario
-            flash('Sesión cerrada correctamente')
-            return redirect(url_for('home'))
-        return render_template('logout.html')
-
     return app
-
---
+++ app/userManager.py
@@ -0,0 +1,54 @@
+import re
+from flask import flash, session, redirect, url_for
+from app.utils.SingleConexionBD import SingleConexionBD
+
+class UserManager:
+    def __init__(self):
+        self.db = SingleConexionBD()
+
+    def login_user(self, username, password):
+        user = self.db.verify_user(username, password)
+        if user:
+            session['logged_in'] = True
+            flash('Inicio de sesión exitoso')
+            return True
+        else:
+            flash('Usuario o contraseña incorrectos')
+            return False
+
+    def register_user(self, username, email, password):
+        if not self.validate_password(password):
+            flash('La contraseña debe tener entre 6 y 12 caracteres, al menos una letra mayúscula, una minúscula y un número')
+            return False
+
+        if not self.validate_email(email):
+            flash('Email incorrecto. Debe ser de la forma: ----@----.es o ----@----.com')
+            return False
+
+        if self.db.insert_newUser(username, email, password):
+            flash('Usuario registrado correctamente. ¡Bienvenid@! Inicia sesión para continuar')
+            return True
+        else:
+            flash('Error al registrar el usuario')
+            return False
+
+    def logout_user(self):
+        session.pop('logged_in', None)
+        flash('Sesión cerrada correctamente')
+
+    @staticmethod
+    def validate_email(email):
+        pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(es|com)$'
+        return re.match(pattern, email) is not None
+
+    @staticmethod
+    def validate_password(password):
+        if len(password) < 6 or len(password) > 12:
+            return False
+        if not re.search("[a-z]", password):
+            return False
+        if not re.search("[A-Z]", password):
+            return False
+        if not re.search("[0-9]", password):
+            return False
+        return True
